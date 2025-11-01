15d3c7c7b77729aef85f7cb40591c95b908865b3
Oleg Chausov <olegshak1985@gmail.com>
2025-11-02 02:01:23 +0300

    alembic is done

---
+++ alembic/env.py
@@ -0,0 +1,43 @@
+from logging.config import fileConfig
+from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine
+from alembic import context
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Event_Pulse_app.db_base import Base
+from Event_Pulse_app import models
+
+config = context.config
+fileConfig(config.config_file_name)
+target_metadata = Base.metadata
+
+def run_migrations_offline():
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = create_engine(
+        config.get_main_option("sqlalchemy.url"),
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
