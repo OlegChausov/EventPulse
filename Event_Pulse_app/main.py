a7614658a0219524bc3b740b265e4b0ad77a938c
Oleg Chausov <olegshak1985@gmail.com>
2025-11-15 22:51:08 +0300

    round parsing and Events creating is done!

--- Event_Pulse_app/main.py
+++ Event_Pulse_app/main.py
@@ -2,9 +2,12 @@ import asyncio
 import sys
 from fastapi import FastAPI
 from sqlalchemy import TryCast
+from sqlalchemy.ext.asyncio import AsyncSession
 
+
+from Event_Pulse_app.utils.get_events_for_user import create_events
 from Event_Pulse_app.utils.parse_all import run_all_parsers
-
+from sqlalchemy.future import select
 from Event_Pulse_app.config import STATIC_DIR
 from Event_Pulse_app.routers import all_routers
 from Event_Pulse_app.database import init_db
@@ -12,8 +15,8 @@ from Event_Pulse_app.middleware.auth_middleware import JWTMiddleware
 from Event_Pulse_app.middleware.token_expiration_moddleware import SlidingExpirationMiddleware
 from fastapi.staticfiles import StaticFiles
 from Event_Pulse_app.config import STATIC_DIR, PARSER_REFRESH_INTERVAL_SECONDS
-
-
+from Event_Pulse_app.models import User
+from Event_Pulse_app.database import AsyncSessionLocal
 
 if sys.platform.startswith("win"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -32,16 +35,28 @@ for r in all_routers:
     app.include_router(r)
 
 
+
 async def parser_loop():
     while True:
-        try:
-            app.state.parsed_events = await run_all_parsers()
-            # print(app.state.parsed_events)
-            await asyncio.sleep(PARSER_REFRESH_INTERVAL_SECONDS)
-        except Exception as e:
-            print(f"❌ Ошибка: {type(e).__name__}: {e}")
-        await asyncio.sleep(PARSER_REFRESH_INTERVAL_SECONDS)
+        async with AsyncSessionLocal() as db:
+            try:
+                app.state.parsed_events = await run_all_parsers()
+                parsed_events = app.state.parsed_events
+                await asyncio.sleep(2)
 
+                result = await db.execute(select(User))
+                users = result.scalars().all()
+
+                for user in users:
+                    try:
+                        await create_events(user, parsed_events, db)
+                    except Exception as e:
+                        print(f"❌ Ошибка: {type(e).__name__}: {e} для {user.id} {user.name}")
+
+            except Exception as e:
+                print(f"❌ Ошибка: {type(e).__name__}: {e}")
+            finally:
+                await asyncio.sleep(PARSER_REFRESH_INTERVAL_SECONDS)
 
 @app.on_event("startup")
 async def startup():
