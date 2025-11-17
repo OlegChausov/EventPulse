ee780db1039a19132b83c1f4178856fbfc7677e0
Oleg Chausov <olegshak1985@gmail.com>
2025-11-17 21:10:16 +0300

    parser_loop is in new file now

--- Event_Pulse_app/main.py
+++ Event_Pulse_app/main.py
@@ -1,22 +1,21 @@
+from fastapi import FastAPI
+app = FastAPI()
 import asyncio
 import sys
-from fastapi import FastAPI
 from sqlalchemy import TryCast
 from sqlalchemy.ext.asyncio import AsyncSession
-
-
 from Event_Pulse_app.utils.get_events_for_user import create_events
 from Event_Pulse_app.utils.parse_all import run_all_parsers
 from sqlalchemy.future import select
 from Event_Pulse_app.config import STATIC_DIR
 from Event_Pulse_app.routers import all_routers
-from Event_Pulse_app.database import init_db
 from Event_Pulse_app.middleware.auth_middleware import JWTMiddleware
 from Event_Pulse_app.middleware.token_expiration_moddleware import SlidingExpirationMiddleware
 from fastapi.staticfiles import StaticFiles
-from Event_Pulse_app.config import STATIC_DIR, PARSER_REFRESH_INTERVAL_SECONDS
-from Event_Pulse_app.models import User
-from Event_Pulse_app.database import AsyncSessionLocal
+from Event_Pulse_app.config import STATIC_DIR
+
+from Event_Pulse_app.parser_loop import parser_loop
+
 
 if sys.platform.startswith("win"):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -27,36 +26,13 @@ app.add_middleware(SlidingExpirationMiddleware)
 app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 
 
-# @app.on_event("startup")
-# async def on_startup():
-#     await init_db()
+
 
 for r in all_routers:
     app.include_router(r)
 
 
 
-async def parser_loop():
-    while True:
-        async with AsyncSessionLocal() as db:
-            try:
-                app.state.parsed_events = await run_all_parsers()
-                parsed_events = app.state.parsed_events
-                await asyncio.sleep(2)
-
-                result = await db.execute(select(User))
-                users = result.scalars().all()
-
-                for user in users:
-                    try:
-                        await create_events(user, parsed_events, db)
-                    except Exception as e:
-                        print(f"❌ Ошибка: {type(e).__name__}: {e} для {user.id} {user.name}")
-
-            except Exception as e:
-                print(f"❌ Ошибка: {type(e).__name__}: {e}")
-            finally:
-                await asyncio.sleep(PARSER_REFRESH_INTERVAL_SECONDS)
 
 @app.on_event("startup")
 async def startup():
--- Event_Pulse_app/parser_loop.py
+++ Event_Pulse_app/parser_loop.py
@@ -1,3 +1,38 @@
+import asyncio
+from Event_Pulse_app.utils.get_events_for_user import create_events
+from Event_Pulse_app.utils.parse_all import run_all_parsers
+from sqlalchemy.future import select
+from Event_Pulse_app.config import PARSER_REFRESH_INTERVAL_SECONDS
+from Event_Pulse_app.models import User
+from Event_Pulse_app.database import AsyncSessionLocal
+
+from Event_Pulse_app.main import app
+
+
+
+
+async def parser_loop():
+    while True:
+        async with AsyncSessionLocal() as db:
+            try:
+                app.state.parsed_events = await run_all_parsers()
+                parsed_events = app.state.parsed_events
+                await asyncio.sleep(2)
+
+                result = await db.execute(select(User))
+                users = result.scalars().all()
+
+                for user in users:
+                    try:
+                        await create_events(user, parsed_events, db)
+                    except Exception as e:
+                        print(f"❌ Ошибка: {type(e).__name__}: {e} для {user.id} {user.name}")
+
+            except Exception as e:
+                print(f"❌ Ошибка: {type(e).__name__}: {e}")
+            finally:
+                await asyncio.sleep(PARSER_REFRESH_INTERVAL_SECONDS)
+
 
 
 
