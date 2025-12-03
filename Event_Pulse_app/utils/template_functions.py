5733c18e212df713c7e5b4d1c0fb69b2ad328c82
Oleg Chausov <olegshak1985@gmail.com>
2025-12-04 00:20:41 +0300

    partials are translated

--- Event_Pulse_app/utils/template_functions.py
+++ Event_Pulse_app/utils/template_functions.py
@@ -1,5 +1,8 @@
 from starlette.templating import Jinja2Templates
-
+from Event_Pulse_app.utils.translations import translations
 from Event_Pulse_app.config import TEMPLATES_DIR
 
 templates = Jinja2Templates(directory=str(TEMPLATES_DIR))
+
+# пока жёстко задаём язык
+templates.env.globals["t"] = translations["en"]
